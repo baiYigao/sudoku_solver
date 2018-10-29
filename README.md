3f36cd0f0feee7ea96875d1b578143b2930ff919
Baiyi Gao <baiyigao@Baiyis-MacBook-Pro.local>
2018-10-29 15:45:23 -0700

    backtracking approach complete

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Sudoku Solver
 
-A program that solves any Sudoku games. Input is a valid Sudoku board represented by a string in Python. Output is a valid filled Sudoku board also represented by represented by a directory. Visualization tool provided.
+## Description
+
+A program that solves any Sudoku games. The input is a valid Sudoku board represented by a string in Python. The output is a valid filled Sudoku board with visualization. The current version uses a simple backtracking method which tries every number for each unfilled squares. More optimizations to come.
+
+## How to run
+
+`python unit_test.py`
+
+Currently there is only one esay test in unit test that you can try out.
--- solve.py
+++ solve.py
@@ -1,13 +1,15 @@
-"""Constraint Propagation approach to solve Sudoku puzzle
+"""Backtracking approach to solve Sudoku puzzle
 
 input: a string of numbers representing the 9x9 board from top left to 
 bottom right (81 number of characters). 0 represents empty square.
 
-output: a visual board that is filled according to Sudoku puzzle rules.
+output: a visual board that is correctly filled according to Sudoku puzzle rules.
 
 """
 
 def cross_product(A, B):
+	"""Return a list that is the cross product of A and B
+	"""
 	return [a + b for a in A for b in B]
 
 class Grid:
@@ -24,13 +26,15 @@ class Grid:
 		self.digits = "123456789"
 		self.rows = "ABCDEFGHI"
 		self.columns = "123456789"
-		self.squares = cross_product(self.rows, self.columns)
+		self.squares = cross_product(self.rows, self.columns) #squares (represented by) labels sorted by rows going from top to bottom and left to right
 		for i in range(len(self.squares)):
-			self.board[self.squares[i]] = s[i]
-		self.unit_list = [cross_product(self.rows, c) for c in self.columns] + [cross_product(r, self.columns) for r in self.rows] + [cross_product(rs, cs) for rs in ["ABC", "DEF", "GHI"] for cs in ["123", "456", "789"]]
+			self.board[self.squares[i]] = s[i] #store square label to value mapping
+		self.unit_list = ([cross_product(self.rows, c) for c in self.columns] +
+						 [cross_product(r, self.columns) for r in self.rows] +
+						 [cross_product(rs, cs) for rs in ["ABC", "DEF", "GHI"] for cs in ["123", "456", "789"]])
 		assert(len(self.unit_list) == 27)
-		self.units = dict((s, [u for u in self.unit_list if s in u]) for s in self.squares)
-		self.peers = dict((s, set(sum(self.units[s], [])) - set([s])) for s in self.squares)
+		self.units = dict((s, [u for u in self.unit_list if s in u]) for s in self.squares) #store a list of units that associate with each square
+		self.peers = dict((s, set(sum(self.units[s], [])) - set([s])) for s in self.squares) #store a set of peers that associate with each sqaure
 
 
 
@@ -44,6 +48,66 @@ class Grid:
 			print("".join(self.board[r + c].center(width) + ("|" if c in "36" else "") for c in self.columns))
 			if r in "CF": print line
 		print
+
+	def find_unassigned(self):
+		"""Find the next unassigned square in the board
+
+		Returns:
+			square label if found; -1 if all squares have filled
+
+		"""
+		for s in self.squares:
+			if self.board[s] == "0":
+				return s
+		else:
+			return -1
+
+	def check_candidate(self, s, c):
+		"""Check if a number is valid for a square position
+		Args:
+			s: label of the square
+			c: the number to fill in
+
+		Returns:
+			True if the number is valid in that square; False if not
+		"""
+		for p in self.peers[s]:
+			if self.board[p] == c:
+				return False
+		return True
+
+
+	def solve(self):
+		"""Solve the puzzle
+		Returns:
+			True if solved; False if not
+		"""
+		square_to_fill = self.find_unassigned()
+		if square_to_fill == -1:
+			return True
+		for candidate in self.digits:
+			if self.check_candidate(square_to_fill, candidate) == False:
+				continue
+			self.board[square_to_fill] = candidate
+			if self.solve():
+				return True
+		self.board[square_to_fill] = "0"
+		return False
+
+
+	def check_valid(self):
+		"""Check if a board is a valid and filled Sudoku board
+		Returns:
+			True if it is valid and filled; False if not
+		"""
+		for s in self.squares:
+			val = self.board[s]
+			for p in self.peers[s]:
+				if self.board[p] == val:
+					return False
+		return True
+
+
 
 
 
